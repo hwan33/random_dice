9e12e87966ef3ac30e7460e1b17b3a22f64fa069
권용환 <hahahaa_a@naver.com>
2023-04-25 23:31:09 +0900

    테마 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:random_dice/const/colors.dart';
 import 'package:random_dice/screen/HomeScreen.dart';
 
 void main() {
   runApp(
     MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: backgroundColor,
+        sliderTheme: SliderThemeData(
+          thumbColor: primaryColor,
+          activeTrackColor: primaryColor,
+          inactiveTrackColor: primaryColor.withOpacity(0.3),
+        ),
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          selectedItemColor: primaryColor,
+          unselectedItemColor: secondaryColor,
+          backgroundColor: backgroundColor,
+        ),
+      ),
       home: HomeScreen(),
     ),
   );
